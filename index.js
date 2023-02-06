01c494a1171249e1a9f9d8c9c4632bf5a529435c
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2023-02-06 16:37:53 -0500

    set up port and one route to test functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.get('/', (req,res) => {
+    res.send("hello this is the initial test ")
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is up and running on Port ${PORT}`)
+})
