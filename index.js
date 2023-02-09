444e67dd50743d596aae9f83d021581fba716e97
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2023-02-08 21:43:35 -0500

    added assets and json data for backend pull requests

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express();
-
+require('dotenv').config()
 const cors = require('cors')
+const fs = require("node:fs")
+
+const getRaces = () =>{
+    const races = fs.readFileSync("./data/circuit_data.json")
+    return JSON.parse(races)
+}
 
 app.use(cors());
+app.use(express.json())
+app.use(express.static("public"))
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT;
 
 app.get('/', (req,res) => {
-    res.send("hello this is the initial test ")
+    const races = getRaces();
+    res.json(races)
 })
+
+
+
 
 app.listen(PORT, () =>{
     console.log(`Server is up and running on Port ${PORT} we are in develop!`)
