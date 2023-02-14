49a5b4b474e54831d39c5d06af309a9781521837
Blaze Slavkovski <Blazeslavkovski@gmail.com>
2023-02-13 19:03:36 -0500

    all functionality working!

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express();
 require('dotenv').config()
 const cors = require('cors')
-const fs = require("node:fs")
+const fs = require("node:fs");
+const { json } = require('express');
 
 const getRaces = () =>{
     const races = fs.readFileSync("./data/circuit_data.json")
     return JSON.parse(races)
+}
+
+const getFollowing = () =>{
+    const following = fs.readFileSync("./data/following_data.json")
+    
+    return JSON.parse(following)
 }
 
 app.use(cors());
@@ -20,11 +27,44 @@ app.get('/', (req,res) => {
     res.json(races)
 })
 
+app.get('/following', (req,res) => {
+    const following = getFollowing();
+    res.json(following)
+})
+
 app.get('/:names',(req,res) => {
     const races = getRaces();
     const selected = races.find(item => item.Circuit.Location.Locality === req.params.names)
     res.json(selected)
 })
+
+
+
+app.get("/following/:names",(req,res) => {
+    const following = getFollowing();
+    console.log(following)
+    console.log(req.params.names)
+    const selected = following.find(item => item.Circuit.Location.Locality === req.params.names);
+    res.json(selected);
+})
+
+app.put("/following/:names", (req,res) =>{
+    const following = getFollowing()
+    let newFollowing = following.filter(element => {
+        return element.Circuit.Location.Locality !== req.params.names
+    });
+    fs.writeFileSync('./data/following_data.json',JSON.stringify(newFollowing))
+    res.json("Race has been deleted")
+})
+
+app.post('/following' ,(req,res) =>{
+    const following = getFollowing();
+    following.push(req.body)
+    fs.writeFileSync('./data/following_data.json',JSON.stringify(following))
+    res.json("race has been posted")
+})
+
+
 
 
 
